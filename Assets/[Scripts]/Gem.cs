69ed6ee0a05310d759b256c1c93e9a52326fc002
woonhak-kong <woonhak.kong@georgebrown.ca>
2022-01-28 14:06:57 -0500

    comments are deleted

--- Assets/[Scripts]/Gem.cs
+++ Assets/[Scripts]/Gem.cs
@@ -18,7 +18,6 @@ public class Gem : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("enter");
         Destroy(gameObject);
     }
 }
--- Assets/[Scripts]/SpawnManager.cs
+++ Assets/[Scripts]/SpawnManager.cs
@@ -15,7 +15,6 @@ public class SpawnManager : MonoBehaviour
     {
         coroutine = SpawnGems(1.0f);
         StartCoroutine(coroutine);
-
     }
 
     // Update is called once per frame
@@ -25,7 +24,6 @@ public class SpawnManager : MonoBehaviour
         {
             Instantiate(prefab, new Vector3(from.transform.position.x, from.transform.position.y,0), Quaternion.identity);
         }
-        
     }
 
     private IEnumerator SpawnGems(float waitTime)
@@ -33,7 +31,6 @@ public class SpawnManager : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(waitTime);
-            Debug.Log("SpawnGems()");
             Instantiate(prefab, new Vector3(Random.Range(from.transform.position.x, to.transform.position.x) , from.transform.position.y, 0), Quaternion.identity);
         }
     }
